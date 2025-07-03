257a149c8ecb4a475513e9d474cf2bb88ce485b6
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-07-03 17:36:30 +0530

    small changes

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,17 +1,22 @@
+
+
+import { HeartPlus } from "lucide-react";
+
 const LandingPage = () => {
   return (
     <>
       <div className="text-white p-6 container mx-auto mt-20">
         <div className="flex items-center justify-center">
-          <div className="bg-green-600 relative">
+          <div className=" relative">
             <div className="flex">
               <img
                 src="/public/hero.jpg"
                 alt="Hero image"
-                className="w-26 h-26 rounded-full"
+                className="w-28 h-28 rounded-full"
               />
             </div>
-            <span className="absolute -bottom-2 left-1/2 text-sm transform -translate-x-1/2 whitespace-nowrap text-[#1d1d1d] bg-white px-4 py-1 rounded-full shadow-lg">
+            <span className="absolute -bottom-3 left-1/2 text-sm transform -translate-x-1/2 whitespace-nowrap text-[#1d1d1d] bg-white px-3 py-2 rounded-full shadow-lg flex items-center">
+              <HeartPlus className="mr-2 size-5" />
               Medical Coder
             </span>
           </div>
