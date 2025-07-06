e9281000abf966ab52091caf5dec6445fe696c11
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-07-06 17:30:44 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,29 +4,29 @@ const Header = () => {
   return (
     <>
       <div className=" text-white p-6 container mx-auto">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mt-2">
           <div className="font-extrabold text-3xl">
             <a
               href="/"
-              className="text-violet-600 py-1 transition-colors duration-200"
+              className="text-sky-600 py-1 transition-colors duration-200"
             >
               Yakshana T.
             </a>
           </div>
-          <div className="" id="navbar">
-            <ul className="flex space-x-6">
+          <div className="text-xl" id="navbar">
+            <ul className="flex space-x-12">
               <li>
                 <a
                   href="/"
-                  className="py-1 text-md font-semibold text-violet-600 transition-colors duration-200"
+                  className="py-1 text-md font-semibold text-sky-600 transition-colors duration-200"
                 >
                   Home
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  className="py-1 text-md font-semibold hover:text-violet-600 transition-colors duration-200"
+                  href="/about"
+                  className="py-1 text-md font-semibold hover:text-sky-600 transition-colors duration-200"
                 >
                   About
                 </a>
@@ -34,7 +34,7 @@ const Header = () => {
               <li>
                 <a
                   href="#"
-                  className="py-1 text-md font-semibold hover:text-violet-600 transition-colors duration-200"
+                  className="py-1 text-md font-semibold hover:text-sky-600 transition-colors duration-200"
                 >
                   Skills
                 </a>
@@ -42,7 +42,7 @@ const Header = () => {
               <li>
                 <a
                   href="#"
-                  className="py-1 text-md font-semibold hover:text-violet-600 transition-colors duration-200"
+                  className="py-1 text-md font-semibold hover:text-sky-600 transition-colors duration-200"
                 >
                   Contact
                 </a>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,18 +6,18 @@ const LandingPage = () => {
   return (
     <>
       <div className="text-white p-6 container mx-auto mt-20">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center flex-col space-y-12">
           <div className=" relative">
             <div className="flex">
               <img
                 src="/public/hero.jpg"
                 alt="Hero image"
-                className="w-28 h-28 rounded-full"
+                className="w-32 h-32 rounded-full"
               />
             </div>
-            <span className="absolute -bottom-3 left-1/2 text-sm transform -translate-x-1/2 whitespace-nowrap text-[#1d1d1d] bg-white px-3 py-2 rounded-full shadow-lg flex items-center">
-              <HeartPlus className="mr-2 size-5" />
-              Medical Coder
+            <span className="absolute -bottom-4 left-1/2 text-sm transform -translate-x-1/2 whitespace-nowrap text-[#1d1d1d] bg-white px-3 py-2 rounded-2xl shadow-lg flex items-center">
+              <HeartPlus className="mr-2 size-6 text-[#1d1d1d] rounded-full" />
+              <p className="text-base font-semibold">Medical Coder</p>
             </span>
           </div>
           <div className="bg-red-600">Heading</div>
