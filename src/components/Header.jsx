b9e8b61370cff4d142ba69ec045236d4b58fd622
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-07-07 10:14:09 +0530

    header alignment chaged

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,46 +13,48 @@ const Header = () => {
               Yakshana T.
             </a>
           </div>
-          <div className="text-xl" id="navbar">
-            <ul className="flex space-x-12">
-              <li>
-                <a
-                  href="/"
-                  className="py-1 font-medium text-sky-600 hover:text-sky-500 transition-colors duration-200"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/about"
-                  className="py-1 font-medium hover:text-sky-500 transition-colors duration-200"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/skills"
-                  className="py-1 font-medium hover:text-sky-500 transition-colors duration-200"
-                >
-                  Skills
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/review"
-                  className="py-1 font-medium hover:text-sky-500 transition-colors duration-200"
-                >
-                  Review
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="text-xl font-medium bg-sky-600 px-4 py-1 rounded-xl hover:bg-sky-800 transition-colors duration-200 cursor-pointer">
-            <a href="/contact" className="">
-              Contact me
-            </a>
+          <div className="flex justify-center items-center gap-12">
+            <div className="text-xl" id="navbar">
+              <ul className="flex space-x-12">
+                <li>
+                  <a
+                    href="/"
+                    className="py-1 font-medium text-sky-600 hover:text-sky-500 transition-colors duration-200"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/about"
+                    className="py-1 font-medium hover:text-sky-500 transition-colors duration-200"
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/skills"
+                    className="py-1 font-medium hover:text-sky-500 transition-colors duration-200"
+                  >
+                    Skills
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/review"
+                    className="py-1 font-medium hover:text-sky-500 transition-colors duration-200"
+                  >
+                    Review
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="text-xl font-medium outline outline-white px-4 py-1 rounded-xl hover:bg-sky-500 hover:outline-sky-500 hover:text-[#1d1d1d] transition-colors duration-200 cursor-pointer">
+              <a href="/contact" className="">
+                Contact me
+              </a>
+            </div>
           </div>
         </div>
       </div>
